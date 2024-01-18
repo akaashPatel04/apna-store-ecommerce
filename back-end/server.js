451faf09ea9a7ccb4fcc94f324361b752f6ebbfc
akaashPatel04 <akaashpatel04@gmail.com>
2024-01-18 16:47:28 +0530

    app and server joined

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,56 @@
-import { app, connectDB } from "./app.js";
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+import cors from "cors";
+import path from 'path'
 import dotenv from 'dotenv';
 dotenv.config({ path: './back-end/.env' });
 
+
+//Route Imports
+import productRouter from "./routes/productRoute.js";
+import userRouter from "./routes/userRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+import paymentRouter from "./routes/paymentRoute.js";
+
+
+const __dirname = path.resolve()
+
+
+//Middlewares
+app.use(express.json())
+app.use(cors())
+
+
+//Routes
+app.use('/product', productRouter)
+app.use('/user', userRouter)
+app.use('/order', orderRouter)
+app.use('/payment', paymentRouter)
+
+
+app.use('/back-end/uploads', express.static("back-end/uploads"))
+
+
+app.use(express.static(path.join(__dirname, '/front-end/build')))
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'front-end', 'build', 'index.html'))
+})
+
+
 //Data Base connection
+export const connectDB = async () => {
+    mongoose.connect(process.env.MONGO)
+        .then((data) => {
+            console.log(`Connected -- apna DB`);
+        })
+        .catch((err) => {
+            console.log(`Failed to connect with DB ${err}`);
+        })
+}
 connectDB()
 
-app.listen(process.env.PORT, () => {
-    console.log(`Port running on http://apna`);
-})
+
+app.listen(process.env.PORT)
